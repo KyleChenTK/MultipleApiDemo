c6e795844a7279de5ed8b89253324b6f2367257a
Kyle Chen <Kyle.tk.Chen@viewsonic.com>
2022-07-22 20:00:56 +0800

    update success

--- MultipleApiDemo/ConfigJson.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ConfigJson.swift
-//  MultipleApiDemo
-//
-//  Created by Kyle Chen on 2022/7/18.
-//
-
-import Foundation
---
+++ MultipleApiDemo/ConfigJson.swift
@@ -0,0 +1,23 @@
+//
+//  ConfigJson.swift
+//  MultipleApiDemo
+//
+//  Created by Kyle Chen on 2022/7/22.
+//
+import Foundation
+
+final class UserManageData: Codable {
+    
+    var uid:Int?
+    
+    var status:String?
+    
+    var message:String?
+    
+    init(uid:Int){
+
+        self.uid = uid
+
+    }
+
+}
